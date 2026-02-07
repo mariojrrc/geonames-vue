786fe83a9ce4db7834e78c554d1414a10b4456ef
Mario Costa <mariojr.rcosta@gmail.com>
2026-02-07 21:41:04 +0000

    fix local api server

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,11 @@ module.exports = {
   devServer: {
     allowedHosts: 'all',
     port: 8081,
+    proxy: {
+      '/v1': {
+        target: process.env.VUE_APP_API_URL || 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
   },
 };
